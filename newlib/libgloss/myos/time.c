73f5f1fb057faa4c37127d10c9687b1877d5674c
davide-matasci <46810856+davide-matasci@users.noreply.github.com>
2026-08-31 21:55:12 +0200

    Drop extra libgloss libc; keep syscall glue (pipe/dup2/waitpid) and rely on newlib + oksh feature-off patches.

--- newlib/libgloss/myos/time.c
+++ newlib/libgloss/myos/time.c
@@ -1,26 +1,33 @@
-/* myos libgloss: clock_gettime from _gettimeofday (libc owns time/localtime). */
+/* myos libgloss: minimal time(3) helpers (epoch from _gettimeofday). */
 
-#include <errno.h>
 #include <time.h>
 #include <sys/time.h>
-#include <sys/types.h>
 
-int
-clock_gettime(clockid_t clock_id, struct timespec *tp)
+time_t
+time(time_t *t)
 {
     struct timeval tv;
 
-    (void)clock_id;
-    if (tp == NULL) {
-        errno = EINVAL;
-        return -1;
-    }
     if (gettimeofday(&tv, NULL) != 0) {
-        tp->tv_sec = 0;
-        tp->tv_nsec = 0;
+        if (t != NULL) {
+            *t = 0;
+        }
         return 0;
     }
-    tp->tv_sec = (time_t)tv.tv_sec;
-    tp->tv_nsec = (long)tv.tv_usec * 1000L;
-    return 0;
+    if (t != NULL) {
+        *t = (time_t)tv.tv_sec;
+    }
+    return (time_t)tv.tv_sec;
+}
+
+struct tm *
+localtime(const time_t *tp)
+{
+    static struct tm result;
+    time_t sec = tp != NULL ? *tp : 0;
+
+    if (gmtime_r(&sec, &result) == NULL) {
+        return NULL;
+    }
+    return &result;
 }
