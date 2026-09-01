de0632bb2bfc697fb458278cd9aac3c7022d5289
davide-matasci <46810856+davide-matasci@users.noreply.github.com>
2026-09-01 04:02:04 +0200

    kernel: task/mod.rs include body_a + body_b only

--- kernel/src/task/mod.rs
+++ kernel/src/task/mod.rs
@@ -1,5 +1,2 @@
-include!("mod_part1a.rs");
-include!("mod_part1b.rs");
-include!("mod_part2.rs");
-include!("mod_part3a.rs");
-include!("mod_part3b.rs");
+include!("body_a.rs");
+include!("body_b.rs");
