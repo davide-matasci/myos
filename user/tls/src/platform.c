84b52d06bfd3b4ad22559c50cf0e57984835b0ce
davide-matasci <46810856+davide-matasci@users.noreply.github.com>
2026-09-05 22:47:42 +0000

    fix(tls): memory_buffer_alloc_init already installs calloc/free

--- user/tls/src/platform.c
+++ user/tls/src/platform.c
@@ -64,10 +64,8 @@ int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t 
 
 static void ensure_mem(void) {
     if (!g_mem_ready) {
+        /* init also installs mbedtls_calloc/free to the buffer allocator */
         mbedtls_memory_buffer_alloc_init(g_heap, sizeof(g_heap));
-        mbedtls_platform_set_calloc_free(
-            mbedtls_memory_buffer_alloc_calloc,
-            mbedtls_memory_buffer_alloc_free);
         g_mem_ready = 1;
     }
 }
