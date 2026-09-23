bdb87d5a34ec4d75c29751e2c20140d7cc0dbebd
davide-matasci <46810856+davide-matasci@users.noreply.github.com>
2026-09-23 04:17:36 +0000

    feat(newlib): POSIX/XSI limits block + all 126 limits tests in the boot set
    patch.sh appends a myos-posix-limits block to newlib limits.h: every
    os-test limits-suite constant (AIO_*, BC_*, CHARCLASS_NAME_MAX, CHILD_MAX,
    COLL_WEIGHTS_MAX, EXPR_NEST_MAX, FILESIZEBITS, GETENTROPY_MAX,
    HOST_NAME_MAX, IOV_MAX/_XOPEN_IOV_MAX, LINK_MAX, LINE_MAX, LOGIN_NAME_MAX,
    LONG_BIT, MAX_CANON, MAX_INPUT, MQ_*, NAME_MAX, NL_*, NZERO, OPEN_MAX,
    PAGESIZE/PAGE_SIZE, PTHREAD_*, RE_DUP_MAX, RTSIG_MAX, SEM_*, SIGQUEUE_MAX,
    SSIZE_MAX, SS_REPL_MAX, STREAM_MAX, SYMLINK_MAX, SYMLOOP_MAX,
    TEXTDOMAIN_MAX + _POSIX_TEXTDOMAIN_MAX, TIMER_MAX, TTY_NAME_MAX,
    TZNAME_MAX, WORD_BIT, _XOPEN_NAME/PATH/TEXTDOMAIN_MAX) with values >= the
    POSIX minimums the suite enforces; OPEN_MAX/CHILD_MAX mirror kernel
    MAX_FDS/MAX_TASKS. The version hash covers patch.sh, so CI rebuilds the
    sysroot instead of the baked-image stale copy.
    
    ci-nonbasic-extra.tests: +21 limits tests that were excluded only because
    their #ifdef-true branch referenced these macros; limits coverage 105->126.
    Boot list is now 423 tests.

--- toolchain/newlib/patch.sh
+++ toolchain/newlib/patch.sh
@@ -323,6 +323,334 @@ PYVALIST
   done
 }
 
+patch_limits_myos() {
+  # POSIX limit macros for myos. The sortix/os-test limits suite compiles
+  # each test as `#ifdef X ... X >= required-minimum ... #else missing_optional`
+  # and expects the constants to exist with honest values. Without this block
+  # ~100 tests either fail to compile (macros referenced inside the #ifdef
+  # branch) or fail at runtime with "missing_optional". Values mirror kernel
+  # reality where measurable (OPEN_MAX=MAX_FDS, CHILD_MAX=MAX_TASKS).
+  local f="$NEWLIB_SRC/newlib/libc/include/limits.h"
+  if ! grep -q 'myos-posix-limits-begin' "$f"; then
+    python3 - "$f" <<'PYLIMITS'
+import sys
+f = sys.argv[1]
+s = open(f).read()
+block = r'''
+/* myos-posix-limits-begin: POSIX/XSI limit constants for myos. The os-test
+   limits suite verifies each is declared with at least its POSIX minimum. */
+#ifndef _POSIX_AIO_LISTIO_MAX
+#define _POSIX_AIO_LISTIO_MAX	2
+#endif
+#ifndef _POSIX_AIO_MAX
+#define _POSIX_AIO_MAX		1
+#endif
+#ifndef _POSIX_ARG_MAX
+#define _POSIX_ARG_MAX		4096
+#endif
+#ifndef _POSIX_CHILD_MAX
+#define _POSIX_CHILD_MAX	25
+#endif
+#ifndef _POSIX_CLOCKRES_MIN
+#define _POSIX_CLOCKRES_MIN	20000000
+#endif
+#ifndef _POSIX_DELAYTIMER_MAX
+#define _POSIX_DELAYTIMER_MAX	32
+#endif
+#ifndef _POSIX_HOST_NAME_MAX
+#define _POSIX_HOST_NAME_MAX	255
+#endif
+#ifndef _POSIX_LINK_MAX
+#define _POSIX_LINK_MAX		8
+#endif
+#ifndef _POSIX_LOGIN_NAME_MAX
+#define _POSIX_LOGIN_NAME_MAX	9
+#endif
+#ifndef _POSIX_MAX_CANON
+#define _POSIX_MAX_CANON	255
+#endif
+#ifndef _POSIX_MAX_INPUT
+#define _POSIX_MAX_INPUT	255
+#endif
+#ifndef _POSIX_MQ_OPEN_MAX
+#define _POSIX_MQ_OPEN_MAX	8
+#endif
+#ifndef _POSIX_MQ_PRIO_MAX
+#define _POSIX_MQ_PRIO_MAX	32
+#endif
+#ifndef _POSIX_NAME_MAX
+#define _POSIX_NAME_MAX		14
+#endif
+#ifndef _POSIX_NGROUPS_MAX
+#define _POSIX_NGROUPS_MAX	8
+#endif
+#ifndef _POSIX_OPEN_MAX
+#define _POSIX_OPEN_MAX		20
+#endif
+#ifndef _POSIX_PATH_MAX
+#define _POSIX_PATH_MAX		256
+#endif
+#ifndef _POSIX_PIPE_BUF
+#define _POSIX_PIPE_BUF		512
+#endif
+#ifndef _POSIX_RE_DUP_MAX
+#define _POSIX_RE_DUP_MAX	255
+#endif
+#ifndef _POSIX_RTSIG_MAX
+#define _POSIX_RTSIG_MAX	8
+#endif
+#ifndef _POSIX_SEM_NSEMS_MAX
+#define _POSIX_SEM_NSEMS_MAX	256
+#endif
+#ifndef _POSIX_SEM_VALUE_MAX
+#define _POSIX_SEM_VALUE_MAX	32767
+#endif
+#ifndef _POSIX_SIGQUEUE_MAX
+#define _POSIX_SIGQUEUE_MAX	32
+#endif
+#ifndef _POSIX_SSIZE_MAX
+#define _POSIX_SSIZE_MAX	32767
+#endif
+#ifndef _POSIX_SS_REPL_MAX
+#define _POSIX_SS_REPL_MAX	4
+#endif
+#ifndef _POSIX_STREAM_MAX
+#define _POSIX_STREAM_MAX	8
+#endif
+#ifndef _POSIX_SYMLINK_MAX
+#define _POSIX_SYMLINK_MAX	255
+#endif
+#ifndef _POSIX_SYMLOOP_MAX
+#define _POSIX_SYMLOOP_MAX	8
+#endif
+#ifndef _POSIX_TEXTDOMAIN_MAX
+#define _POSIX_TEXTDOMAIN_MAX	1024
+#endif
+#ifndef _POSIX_THREAD_DESTRUCTOR_ITERATIONS
+#define _POSIX_THREAD_DESTRUCTOR_ITERATIONS	4
+#endif
+#ifndef _POSIX_THREAD_KEYS_MAX
+#define _POSIX_THREAD_KEYS_MAX	128
+#endif
+#ifndef _POSIX_THREAD_THREADS_MAX
+#define _POSIX_THREAD_THREADS_MAX	64
+#endif
+#ifndef _POSIX_TIMER_MAX
+#define _POSIX_TIMER_MAX	32
+#endif
+#ifndef _POSIX_TTY_NAME_MAX
+#define _POSIX_TTY_NAME_MAX	9
+#endif
+#ifndef _POSIX_TZNAME_MAX
+#define _POSIX_TZNAME_MAX	6
+#endif
+#ifndef _POSIX2_BC_BASE_MAX
+#define _POSIX2_BC_BASE_MAX	99
+#endif
+#ifndef _POSIX2_BC_DIM_MAX
+#define _POSIX2_BC_DIM_MAX	255
+#endif
+#ifndef _POSIX2_BC_SCALE_MAX
+#define _POSIX2_BC_SCALE_MAX	99
+#endif
+#ifndef _POSIX2_BC_STRING_MAX
+#define _POSIX2_BC_STRING_MAX	255
+#endif
+#ifndef _POSIX2_CHARCLASS_NAME_MAX
+#define _POSIX2_CHARCLASS_NAME_MAX	14
+#endif
+#ifndef _POSIX2_COLL_WEIGHTS_MAX
+#define _POSIX2_COLL_WEIGHTS_MAX	255
+#endif
+#ifndef _POSIX2_EXPR_NEST_MAX
+#define _POSIX2_EXPR_NEST_MAX	32
+#endif
+#ifndef _POSIX2_LINE_MAX
+#define _POSIX2_LINE_MAX	2048
+#endif
+#ifndef _XOPEN_IOV_MAX
+#define _XOPEN_IOV_MAX		16
+#endif
+#ifndef _XOPEN_NAME_MAX
+#define _XOPEN_NAME_MAX		255
+#endif
+#ifndef _XOPEN_PATH_MAX
+#define _XOPEN_PATH_MAX		1024
+#endif
+#ifndef _XOPEN_TEXTDOMAIN_MAX
+#define _XOPEN_TEXTDOMAIN_MAX	1024
+#endif
+
+#ifndef AIO_LISTIO_MAX
+#define AIO_LISTIO_MAX		2
+#endif
+#ifndef AIO_MAX
+#define AIO_MAX			1
+#endif
+#ifndef AIO_PRIO_DELTA_MAX
+#define AIO_PRIO_DELTA_MAX	0
+#endif
+#ifndef ATEXIT_MAX
+#define ATEXIT_MAX		32
+#endif
+#ifndef BC_BASE_MAX
+#define BC_BASE_MAX		99
+#endif
+#ifndef BC_DIM_MAX
+#define BC_DIM_MAX		255
+#endif
+#ifndef BC_SCALE_MAX
+#define BC_SCALE_MAX		99
+#endif
+#ifndef BC_STRING_MAX
+#define BC_STRING_MAX		255
+#endif
+#ifndef CHARCLASS_NAME_MAX
+#define CHARCLASS_NAME_MAX	14
+#endif
+/* CHILD_MAX: kernel MAX_TASKS. */
+#ifndef CHILD_MAX
+#define CHILD_MAX		32
+#endif
+#ifndef COLL_WEIGHTS_MAX
+#define COLL_WEIGHTS_MAX	255
+#endif
+#ifndef DELAYTIMER_MAX
+#define DELAYTIMER_MAX		32
+#endif
+#ifndef EXPR_NEST_MAX
+#define EXPR_NEST_MAX		32
+#endif
+#ifndef FILESIZEBITS
+#define FILESIZEBITS		64
+#endif
+#ifndef GETENTROPY_MAX
+#define GETENTROPY_MAX		256
+#endif
+#ifndef HOST_NAME_MAX
+#define HOST_NAME_MAX		255
+#endif
+#ifndef IOV_MAX
+#define IOV_MAX			1024
+#endif
+#ifndef LINE_MAX
+#define LINE_MAX		2048
+#endif
+#ifndef LINK_MAX
+#define LINK_MAX		128
+#endif
+#ifndef LOGIN_NAME_MAX
+#define LOGIN_NAME_MAX		256
+#endif
+#ifndef LONG_BIT
+#define LONG_BIT		64
+#endif
+#ifndef MAX_CANON
+#define MAX_CANON		255
+#endif
+#ifndef MAX_INPUT
+#define MAX_INPUT		255
+#endif
+#ifndef MQ_OPEN_MAX
+#define MQ_OPEN_MAX		8
+#endif
+#ifndef MQ_PRIO_MAX
+#define MQ_PRIO_MAX		32
+#endif
+#ifndef NAME_MAX
+#define NAME_MAX		255
+#endif
+#ifndef NL_LANGMAX
+#define NL_LANGMAX		32
+#endif
+#ifndef NL_MSGMAX
+#define NL_MSGMAX		32767
+#endif
+#ifndef NL_SETMAX
+#define NL_SETMAX		255
+#endif
+#ifndef NL_TEXTMAX
+#define NL_TEXTMAX		2048
+#endif
+#ifndef NZERO
+#define NZERO			20
+#endif
+/* OPEN_MAX: kernel MAX_FDS. */
+#ifndef OPEN_MAX
+#define OPEN_MAX		32
+#endif
+#ifndef PAGESIZE
+#define PAGESIZE		4096
+#endif
+#ifndef PAGE_SIZE
+#define PAGE_SIZE		4096
+#endif
+#ifndef PTHREAD_DESTRUCTOR_ITERATIONS
+#define PTHREAD_DESTRUCTOR_ITERATIONS	4
+#endif
+#ifndef PTHREAD_KEYS_MAX
+#define PTHREAD_KEYS_MAX	128
+#endif
+#ifndef PTHREAD_STACK_MIN
+#define PTHREAD_STACK_MIN	16384
+#endif
+#ifndef PTHREAD_THREADS_MAX
+#define PTHREAD_THREADS_MAX	64
+#endif
+#ifndef RE_DUP_MAX
+#define RE_DUP_MAX		255
+#endif
+#ifndef RTSIG_MAX
+#define RTSIG_MAX		32
+#endif
+#ifndef SEM_NSEMS_MAX
+#define SEM_NSEMS_MAX		256
+#endif
+#ifndef SEM_VALUE_MAX
+#define SEM_VALUE_MAX		32767
+#endif
+#ifndef SIGQUEUE_MAX
+#define SIGQUEUE_MAX		32
+#endif
+#ifndef SSIZE_MAX
+#define SSIZE_MAX		((long long)(~((unsigned long long)0 >> 1)))
+#endif
+#ifndef SS_REPL_MAX
+#define SS_REPL_MAX		4
+#endif
+#ifndef STREAM_MAX
+#define STREAM_MAX		16
+#endif
+#ifndef SYMLINK_MAX
+#define SYMLINK_MAX		255
+#endif
+#ifndef SYMLOOP_MAX
+#define SYMLOOP_MAX		8
+#endif
+#ifndef TEXTDOMAIN_MAX
+#define TEXTDOMAIN_MAX		1024
+#endif
+#ifndef TIMER_MAX
+#define TIMER_MAX		32
+#endif
+#ifndef TTY_NAME_MAX
+#define TTY_NAME_MAX		32
+#endif
+#ifndef TZNAME_MAX
+#define TZNAME_MAX		32
+#endif
+#ifndef WORD_BIT
+#define WORD_BIT		32
+#endif
+/* myos-posix-limits-end */'''
+s = s.rstrip() + "\n" + block + "\n"
+open(f, "w").write(s)
+PYLIMITS
+    echo "patched limits.h: myos POSIX limits block"
+  fi
+}
+
 patch_valist
 patch_tmpfile
 patch_x86_longjmp_val0
+patch_limits_myos
